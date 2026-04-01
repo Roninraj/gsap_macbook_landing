d981dff40d5e246d7a34504d49baa45b5124afbb
Rahul Raj Soren <rahulsoren90@gmail.com>
2026-04-01 11:21:54 +0530

    Sumanumanimanuma
    Waka Waka eh eh!

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14.jsx";
+import MacbookPro14Model from "./models/Macbook14.jsx";
 
 const ProductViewer = () => {
     const {color,scale,setColor,setScale} = useMacbookStore();
@@ -42,7 +43,7 @@ const ProductViewer = () => {
             </div>
             <Canvas id="canvas" camera={{position:[0,2,5], fov:50, near:0.1, far:100}}>
                 <ambientLight intensity={1}/>
-                <MacbookModel14 scale={0.06} position={[[-1,0,0]]}/>
+                {/*<MacbookPro14Model scale={0.06} position={[[-1,0,0]]} />*/}
                 <OrbitControls enableZoom={false} />
             </Canvas>
         </section>
